ba8994c011ed371252a8dfa4d6d2698fa486821e
Jose Luis Romero <pepe.romeros@gmail.com>
2016-12-07 23:17:58 -0500

    Remove keys from Config.php.

--- include/Config.php
+++ include/Config.php
@@ -5,15 +5,15 @@
  *
  * Database configuration.
  */
-define('DB_USERNAME', 'mmadmin');
-define('DB_PASSWORD', '1Hwhz03q;;');
+define('DB_USERNAME', 'your_user');
+define('DB_PASSWORD', 'your_pass');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'marketme');
+define('DB_NAME', 'yourdbname');
 
 /**
  * BrainTree sandbox configuration.
  */
 define('BT_ENVIRONMENT','sandbox');
-define('BT_MERCHANT_ID', 'kpbj78mvdnjv9r6r'); // BrainTree merchant id
-define('BT_API_SECRET', 'd3djmkbfpksscw6k'); // BraintTree secret
-define('BT_API_KEY','8fdd66e9212da734fed4105d78c1428c'); // BrainTree key
+define('BT_MERCHANT_ID', 'your_merch_id'); // BrainTree merchant id
+define('BT_API_SECRET', 'your_api_secret'); // BraintTree secret
+define('BT_API_KEY','your_api_key'); // BrainTree key
